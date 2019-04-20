c171b8502cd5b48f3c61408bcf7784373cb6b157
Edward Almanzar <eddeveloper@hotmail.com>
2019-04-20 18:51:28 -0400

    Dynamically requesting data on the client at run time

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { graphql , Link } from "gatsby"
+import axios from "axios"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -18,10 +19,52 @@ export const GatsbyQuery = graphql`
   }
 `
 class Index extends Component {
+
+  state = {
+    loading: false,
+    error: false,
+    pupper:{
+      img: "",
+      breed: "",
+    },
+  }
+
+  componentDidMount() {
+    this.fetchRicksPupper()
+  }
+
+  fetchRicksPupper = () =>{
+    this.setState({loading: true})
+    axios
+      .get(`https://dog.ceo/api/breeds/image/random`)
+      .then(pupper =>{
+
+        const {
+          data: { message: img },
+        } = pupper
+
+        const breed = img.split("/")[4]
+
+        this.setState({
+          loading: false,
+          pupper: {
+            ...this.state.pupper,
+            img,
+            breed,
+          },
+        })
+      })
+      .catch(error => {
+        this.setState({ loading: false, error })
+      })
+  }
+
   render() {
       const {
           rickAndMorty: { character },
       } = this.props.data
+
+      const { img, breed } = this.state.pupper
 
     return (
       <Layout>
@@ -30,12 +73,25 @@ class Index extends Component {
       <div style={{ textAlign: "center", width: "600px", margin: "50px auto" }}>
           <h1>{character.name} With his Pupper</h1>
           <p>Rick & Morty API data loads at build time.</p>
+          <p>Dog API data loads at run time.</p>
           <div>
             <img
               src={character.image}
               alt={character.name}
               style={{ width: 300 }}
             />
+          </div>
+          <div>
+             { this.state.loading ? (
+               <p> Please hold, pupper is comming!</p>
+             ) : img && breed ? (
+              <>  
+                <h2> { `${breed} pupper!` }</h2>
+                <img src={img} alt={`cute random `} style={{ maxWidth: 300 }} />
+              </>
+             ) : (
+              <p>Oh noes, error fetching pupper :(</p>
+            )}
           </div>
 
           <h2>Image of Rick's pupper</h2>
